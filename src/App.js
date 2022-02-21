30023a5643e09fd84af6139d1e1cf4746ed21da7
Yeats Goodwin <yeatsg@gmail.com>
2022-02-21 12:33:10 -0500

    yeats styles

--- src/App.js
+++ src/App.js
@@ -10,8 +10,46 @@ import background from "../src/images/background.png";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <nav>
+        <img src={ironhack} class="teeny-watermark" />
+        <img src={menu} class="teeny-watermark" />
+      </nav>
+      <div id="big-boi">
+        <h1>Say hello to ReactJS</h1>
+        <p>
+          You will learn how to use the most popular frontend library, and
+          become a super Ninja developer
+        </p>
+        <a href="#" class="button">
+          Awesome
+        </a>
+      </div>
+      <div id="icon-row">
+        <div class="column">
+          <img src={icon1} alt="image not found" />
+          <h2>Declarative</h2>
+          <p>React makes it painless to create interactive UIs.</p>
+        </div>
+        <div class="column">
+          <img src={icon2} />
+          <h2>Components</h2>
+          <p>React makes it painless to create interactive UIs.</p>
+        </div>
+        <div class="column">
+          <img src={icon3} />
+          <h2>Single-way</h2>
+          <p>React makes it painless to create interactive UIs.</p>
+        </div>
+        <div class="column">
+          <img src={icon4} />
+          <h2>JSX</h2>
+          <p>React makes it painless to create interactive UIs.</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-c;
 export default App;
