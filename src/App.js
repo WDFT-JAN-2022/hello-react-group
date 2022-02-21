d46789427f53f59dec7ce9b70a82d504dca0e009
Sergey Kadiyevskiy <51814805+Kalashnik0v1947@users.noreply.github.com>
2022-02-21 12:23:22 -0500

    iteration 3 serge

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import icon1 from "../src/images/icon1.png";
+import icon2 from "../src/images/icon2.png";
+import icon3 from "../src/images/icon3.png";
+import icon4 from "../src/images/icon4.png";
+import ironhack from "../src/images/ironhack-logo-xs.png";
+import menu from "../src/images/menu-top-xs.png";
+import background from "../src/images/background.png";
+
 // src/App.js
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App"></div>
-  );
+  return <div className="App"></div>;
 }
+
+c;
 export default App;
